c55118a6a179f63b975cb66593e3284e007d3de9
me <me@example.com>
2018-09-21 11:33:44 -0400

    Latest Commit

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 #! /usr/bin/python3
 
 def main(msg):
-<<<<<<< HEAD
-    //No longer need comments here
-    print(msg)
-
-=======
     //No longer need comment here
     print(msg)
     
->>>>>>> v01
 def secondary():
     print("New Function")
     
